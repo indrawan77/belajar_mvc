cf11b0491433897ef9ed835a57c425145d40c09e
banana77 <xenotrone@gmail.com>
2021-01-23 01:56:06 +0700

    Ganti Judul Halaman

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends Controller
   public function index($param1 = 'jembut')
   {
     $data['param1'] = $param1;
-    $data['judul'] = 'INDEX';
+    $data['judul'] = 'DASHBOARD';
     $this->view('templates/header', $data);
     $this->view('home/index', $data);
     $this->view('templates/footer');
